0eb7a456c18c5b68358596d5e88849866b5325f6
Sagexd08 <sohomchattejee07@gmail.com>
2025-04-17 04:24:43 +0530

    Add test page and fix dependencies

---
+++ Frontend/app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold mb-4">Test Page</h1>
+      <p className="text-xl">If you can see this, the deployment is working!</p>
+    </div>
+  );
+}
